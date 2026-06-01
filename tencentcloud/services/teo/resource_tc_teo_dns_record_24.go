dee03ac67dd1e0c5f43f299ada026483352cb22b
iac-agent <tencentcloud_iac@tencent.com>
2026-06-01 14:44:40 +0800

    docs(teo): update tencentcloud_teo_dns_record_24 schema descriptions

--- tencentcloud/services/teo/resource_tc_teo_dns_record_24.go
+++ tencentcloud/services/teo/resource_tc_teo_dns_record_24.go
@@ -28,53 +28,53 @@ func ResourceTencentCloudTeoDnsRecord24() *schema.Resource {
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-			Description: "站点 ID。比输入值1112222333333444qewd",
+			Description: "站点 ID. 比输入值1112222333333444.",
 			},
 
 			"name": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "DNS 记录名，如果是中文、韩文、日文域名，需要转换为 punycode 后输入。",
+				Description: "DNS 记录名，如果是中文、韩文、日文域名，需要转换为 punycode 后输入.",
 			},
 
 			"type": {
-				Type:     schema.TypeString,
-				Required: true,
-				Description: "DNS 记录类型，取值有：<li>A：将域名指向一个外网 IPv4 地址，如 8.8.8.8；</li><li>AAAA：将域名指向一个外网 IPv6 地址；</li><li>MX：用于邮箱服务器。存在多条 MX 记录时，优先级越低越优先；</li><li>CNAME：将域名指向另一个域名，再由该域名解析出最终 IP 地址；</li><li>TXT：对域名进行标识和说明，常用于域名验证和 SPF 记录（反垃圾邮件）；</li><li>NS：如果需要将子域名交给其他 DNS 服务商解析，则需要添加 NS 记录。根域名无法添加 NS 记录；</li><li>CAA：指定可为本站点颁发证书的 CA；</li><li>SRV：标识某台服务器使用了某个服务，常见于微软系统的目录管理。</li>\n不同的记录类型呢例如 SRV、CAA 记录对主机记录名称、记录值格式有不同的要求，各记录类型的详细说明介绍和格式示例请参考：[解析记录类型介绍](https://cloud.tencent.com/document/product/1552/90453#2f681022-91ab-4a9e-ac3d-0a6c454d954e)。",
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "DNS 记录类型，取值有：<li>A：将域名指向一个外网 IPv4 地址，如 8.8.8.8；</li><li>AAAA：将域名指向一个外网 IPv6 地址；</li><li>MX：用于邮箱服务器。存在多条 MX 记录时，优先级越低越优先；</li><li>CNAME：将域名指向另一个域名，再由该域名解析出最终 IP 地址；</li><li>TXT：对域名进行标识和说明，常用于域名验证和 SPF 记录（反垃圾邮件）；</li><li>NS：如果需要将子域名交给其他 DNS 服务商解析，则需要添加 NS 记录。根域名无法添加 NS 记录；</li><li>CAA：指定可为本站点颁发证书的 CA；</li><li>SRV：标识某台服务器使用了某个服务，常见于微软系统的目录管理。</li>\n不同的记录类型呢例如 SRV、CAA 记录对主机记录名称、记录值格式有不同的要求，各记录类型的详细说明介绍和格式示例请参考：[解析记录类型介绍](https://cloud.tencent.com/document/product/1552/90453#2f681022-91ab-4a9e-ac3d-0a6c454d954e).",
 			},
 
 			"content": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "DNS 记录内容，根据 Type 值填入与之相对应的内容，如果是中文、韩文、日文域名，需要转换为 punycode 后输入。",
+				Description: "DNS 记录内容，根据 Type 值填入与之相对应的内容，如果是中文、韩文、日文域名，需要转换为 punycode 后输入.",
 			},
 
 			"location": {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Optional:    true,
-				Description: "DNS 记录解析线路，不指定默认为 Default，表示默认解析线路，代表全部地域生效。\n\n- 解析线路配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。\n- 解析线路配置仅适用于标准版、企业版套餐使用，取值请参考：[解析线路及对应代码枚举](https://cloud.tencent.com/document/product/1552/112542)。",
+				Description: "DNS 记录解析线路，不指定默认为 Default，表示默认解析线路，代表全部地域生效。\n\n- 解析线路配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。\n- 解析线路配置仅适用于标准版、企业版套餐使用，取值请参考：[解析线路及对应代码枚举](https://cloud.tencent.com/document/product/1552/112542).",
 			},
 
 			"ttl": {
 				Type:        schema.TypeInt,
 				Optional:    true,
 				Computed:    true,
-				Description: "缓存时间，用户可指定值范围 60~86400，数值越小，修改记录各地生效时间越快，默认为 300，单位：秒。",
+				Description: "缓存时间，用户可指定值范围 60~86400，数值越小，修改记录各地生效时间越快，默认为 300，单位：秒.",
 			},
 
 			"weight": {
 				Type:        schema.TypeInt,
 				Optional:    true,
 				Computed:    true,
-				Description: "DNS 记录权重，用户可指定值范围 -1~100，设置为 0 时表示不解析，不指定默认为 -1，表示不设置权重。权重配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。<br>注意：同一个子域名下，相同解析线路的不同 DNS 记录，应保持同时设置权重或者同时都不设置权重。",
+				Description: "DNS 记录权重，用户可指定值范围 -1~100，设置为 0 时表示不解析，不指定默认为 -1，表示不设置权重。权重配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。<br>注意：同一个子域名下，相同解析线路的不同 DNS 记录，应保持同时设置权重或者同时都不设置权重.",
 			},
 
 			"priority": {
 				Type:        schema.TypeInt,
 				Optional:    true,
 				Computed:    true,
-				Description: "MX 记录优先级，该参数仅在当 Type（DNS 记录类型）为 MX 时生效，值越小优先级越高，用户可指定值范围0~50，不指定默认为0。",
+				Description: "MX 记录优先级，该参数仅在当 Type（DNS 记录类型）为 MX 时生效，值越小优先级越高，用户可指定值范围0~50，不指定默认为0.",
 			},
 
 			"record_id": {
@@ -106,68 +106,68 @@ func ResourceTencentCloudTeoDnsRecord24() *schema.Resource {
 			"dns_records": {
 				Type:        schema.TypeList,
 				Computed:    true,
-				Description: "DNS 记录列表。",
+				Description: "DNS 记录列表.",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"zone_id": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "站点 ID。<br>注意：ZoneId 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略。",
+							Description: "站点 ID。<br>注意：ZoneId 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略.",
 						},
 						"record_id": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "DNS 记录 ID。",
+							Description: "DNS 记录 ID.",
 						},
 						"name": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "DNS 记录名。",
+							Description: "DNS 记录名.",
 						},
 						"type": {
-							Type:     schema.TypeString,
-							Computed: true,
+							Type:        schema.TypeString,
+							Computed:    true,
 							Description: "DNS 记录类型，取值有：\n<li>A：将域名指向一个外网 IPv4 地址，如 8.8.8.8；</li>\n<li>AAAA：将域名指向一个外网 IPv6 地址；</li>\n<li>MX：用于邮箱服务器。存在多条 MX 记录时，优先级越低越优先；</li>\n<li>CNAME：将域名指向另一个域名，再由该域名解析出最终 IP 地址；</li>\n<li>TXT：对域名进行标识和说明，常用于域名验证和 SPF 记录（反垃圾邮件）；</li>\n<li>NS：如果需要将子域名交给其他 DNS 服务商解析，则需要添加 NS 记录。根域名无法添加 NS 记录；</li>\n<li>CAA：指定可为本站点颁发证书的 CA；</li>\n<li>SRV：标识某台服务器使用了某个服务，常见于微软系统的目录管理。</li>",
 						},
 						"location": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "DNS 记录解析线路，不指定默认为 Default，表示默认解析线路，代表全部地域生效。<br>解析线路配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。<br>取值请参考：[解析线路及对应代码枚举](https://cloud.tencent.com/document/product/1552/112542)。",
+							Description: "DNS 记录解析线路，不指定默认为 Default，表示默认解析线路，代表全部地域生效。<br>解析线路配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。<br>取值请参考：[解析线路及对应代码枚举](https://cloud.tencent.com/document/product/1552/112542).",
 						},
 						"content": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "DNS 记录内容。根据 Type 值填入与之相对应的内容。",
+							Description: "DNS 记录内容。根据 Type 值填入与之相对应的内容.",
 						},
 						"ttl": {
 							Type:        schema.TypeInt,
 							Computed:    true,
-							Description: "缓存时间，取值范围 60~86400，数值越小，修改记录各地生效时间越快，单位：秒。",
+							Description: "缓存时间，取值范围 60~86400，数值越小，修改记录各地生效时间越快，单位：秒.",
 						},
 						"weight": {
 							Type:        schema.TypeInt,
 							Computed:    true,
-							Description: "DNS 记录权重，取值范围 -1~100，为 -1 时表示不分配权重，为 0 时表示不解析。权重配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时。",
+							Description: "DNS 记录权重，取值范围 -1~100，为 -1 时表示不分配权重，为 0 时表示不解析。权重配置仅适用于当 Type（DNS 记录类型）为 A、AAAA、CNAME 时.",
 						},
 						"priority": {
 							Type:        schema.TypeInt,
 							Computed:    true,
-							Description: "MX 记录优先级，取值范围 0~50，数值越小越优先。",
+							Description: "MX 记录优先级，取值范围 0~50，数值越小越优先.",
 						},
 						"status": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "DNS 记录解析状态，取值有：<li>enable：已生效；</li><li>disable：已停用。</li>注意：Status 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略。",
+							Description: "DNS 记录解析状态，取值有：<li>enable：已生效；</li><li>disable：已停用。</li>注意：Status 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略.",
 						},
 						"created_on": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "创建时间。<br>注意：CreatedOn 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略。",
+							Description: "创建时间。<br>注意：CreatedOn 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略.",
 						},
 						"modified_on": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "修改时间。<br>注意：ModifiedOn 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略。",
+							Description: "修改时间。<br>注意：ModifiedOn 仅做出参使用，在 ModifyDnsRecords 不可作为入参使用，如有传此参数，会忽略.",
 						},
 					},
 				},
